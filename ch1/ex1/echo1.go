782aeb196a78dc4fa5eb7789e862648422cd1566
新井昇平 <shohei.arai.52@gmail.com>
2015-12-28 22:14:26 +0900

    Add: ex1.1

---
+++ ch1/ex1/echo1.go
@@ -0,0 +1,18 @@
+// Copyright © 2016 shoarai
+
+// Echo1 prints its command-line name and arguments.
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var s, sep string
+	for i := 0; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+}
