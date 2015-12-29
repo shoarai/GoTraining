c2bc42bce3f777a34ce25dadb40c78c66f6999d0
新井昇平 <shohei.arai.52@gmail.com>
2015-12-29 20:25:17 +0900

    Add: Print names of files

--- ch1/ex4/main.go
+++ ch1/ex4/main.go
@@ -1,7 +1,7 @@
 // Copyright © 2016 shoarai
 
-// Dup prints the count and text of lines that appear more than once
-// in the input and the names of files in which text appears.
+// Dup2 prints the count and text of lines that appear more than once
+// in the input and the names of files in which the text appears.
 // It reads from stdin or from a list of named files.
 package main
 
@@ -9,13 +9,19 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
+type Line struct {
+	Count int
+	Files []string
+}
+
 func main() {
-	counts := make(map[string]int)
+	lines := make(map[string]Line)
 	files := os.Args[1:]
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		countLines(os.Stdin, lines, "")
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -23,21 +29,35 @@ func main() {
 				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
 				continue
 			}
-			countLines(f, counts)
+			countLines(f, lines, arg)
 			f.Close()
 		}
 	}
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+	for line, n := range lines {
+		if n.Count > 1 {
+			fmt.Printf("%d\t%s\t%s\n", n.Count, line, strings.Join(n.Files, " "))
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, lines map[string]Line, filename string) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
-		counts[input.Text()]++
+		count := lines[input.Text()]
+		count.Count++
+		if !stringInStrings(filename, count.Files) {
+			count.Files = append(count.Files, filename)
+		}
+		lines[input.Text()] = count;
 	}
 	// NOTE: ignoring potential errors from input.Err()
 }
+
+func stringInStrings(str string, strs []string) bool {
+ 	for _, s := range strs {
+ 		if s == str {
+ 			return true
+ 		}
+ 	}
+ 	return false
+ }
