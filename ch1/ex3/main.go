88ce33902c9cc85d3f4ca84cc81c0e08b0939b70
新井昇平 <shohei.arai.52@gmail.com>
2016-01-16 11:37:41 +0900

    Add: ex3

---
+++ ch1/ex3/main.go
@@ -0,0 +1,35 @@
+// Copyright © 2016 shoarai
+
+// Echo prints the difference in running time
+// between the inefficient version and the one
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	GetOSArgsInefficient()
+	timeLong := time.Since(start)
+	start = time.Now()
+	GetOSArgs()
+	timeShort := time.Since(start)
+	fmt.Println(timeLong, " - ", timeShort, " = ", timeLong-timeShort)
+}
+
+func GetOSArgsInefficient() string {
+	var s, sep string
+	for i := 0; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	return s
+}
+
+func GetOSArgs() string {
+	return strings.Join(os.Args[0:], " ")
+}
