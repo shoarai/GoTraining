cff375a776b6c24992f36f713be7595fd106f6b3
新井昇平 <shohei.arai.52@gmail.com>
2016-02-12 14:45:38 +0900

    add: ch2/ex1

---
+++ src/ch2/tempconv/conv.go
@@ -0,0 +1,22 @@
+// Copyright © 2016 shoarai
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+package tempconv
+
+// CToF converts a Celsius temperature to Fahrenheit.
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+
+// FToC converts a Fahrenheit temperature to Celsius.
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+
+// KToC converts a Kelvin temperature to Celsius.
+func KToC(k Kelvin) Celsius { return Celsius(k - 273.15) }
+
+// CToK converts a Celsius temperature to Kelvin.
+func CToK(c Celsius) Kelvin { return Kelvin(c + 273.15) }
+
+// KToF converts a Kelvin temperature to Celsius.
+func KToF(k Kelvin) Fahrenheit { return Fahrenheit((k - 273.15)*9/5 + 32) }
+
+// FToK converts a Celsius temperature to Kelvin.
+func FToK(f Fahrenheit) Kelvin { return Kelvin((f - 32) * 5 / 9 + 273.15) }
