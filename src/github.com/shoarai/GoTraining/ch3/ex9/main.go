3972839f5ea491fdd1910a2432e30a1d5650b57d
新井昇平 <shohei.arai.52@gmail.com>
2016-03-03 23:22:01 +0900

    add: axis and scale queries

--- src/github.com/shoarai/GoTraining/ch3/ex9/main.go
+++ src/github.com/shoarai/GoTraining/ch3/ex9/main.go
@@ -2,6 +2,9 @@
 // License: https://creativecommons.org/licenses/by-nc-sa/4.0/
 
 // Mandelbrot emits a PNG image of the Mandelbrot fractal in full color.
+//
+// Mandelbrot is a PNG image of the Mandelbrot server.
+// The queries are "x", "y" of axises and "scale" of the image scale
 package main
 
 import (
@@ -11,15 +14,12 @@ import (
 	"log"
 	"math/cmplx"
 	"net/http"
-	"os"
+	"strconv"
 )
 
 func main() {
-	if len(os.Args) > 1 && os.Args[1] == "web" {
-		http.HandleFunc("/", handler)
-		log.Fatal(http.ListenAndServe("localhost:8000", nil))
-		return
-	}
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -29,36 +29,42 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		log.Print(err)
 	}
 
-	// for k, v := range r.Form {
-	// 	if k == "width" {
-	// 		if val, err := strconv.Atoi(v[0]); err == nil {
-	// 			setWidth(val)
-	// 		}
-	// 	} else if k == "height" {
-	// 		if val, err := strconv.Atoi(v[0]); err == nil {
-	// 			setHeight(val)
-	// 		}
-	// 	} else if k == "cells" {
-	// 		if val, err := strconv.Atoi(v[0]); err == nil {
-	// 			setCells(val)
-	// 		}
-	// 	}
-	// }
-	// fmt.Fprint(w, svg())
-	svg(w)
+	var x, y float64
+	scale := 1.0
+	for k, v := range r.Form {
+		if k == "x" {
+			if val, err := strconv.ParseFloat(v[0], 64); err == nil {
+				x = val
+			}
+		} else if k == "y" {
+			if val, err := strconv.ParseFloat(v[0], 64); err == nil {
+				y = val
+			}
+		} else if k == "scale" {
+			if val, err := strconv.ParseFloat(v[0], 64); err == nil {
+				scale = val
+			}
+		}
+	}
+	svg(w, x, y, scale)
 }
 
-func svg(w http.ResponseWriter) {
+func svg(w http.ResponseWriter, x, y, scale float64) {
 	const (
-		xmin, ymin, xmax, ymax = -2, -2, +2, +2
-		width, height          = 1024, 1024
+		width, height = 1024, 1024
 	)
+
+	base := 2.0
+	xmin := -base / scale
+	ymin := xmin
+	xmax := base / scale
+	ymax := xmax
 
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
 	for py := 0; py < height; py++ {
-		y := float64(py)/height*(ymax-ymin) + ymin
+		y := float64(py)/height*(ymax-ymin) + ymin + y
 		for px := 0; px < width; px++ {
-			x := float64(px)/width*(xmax-xmin) + xmin
+			x := float64(px)/width*(xmax-xmin) + xmin + x
 			z := complex(x, y)
 			// Image point (px, py) represents complex value z.
 			img.Set(px, py, mandelbrot(z))
@@ -77,39 +83,6 @@ func mandelbrot(z complex128) color.Color {
 		v = v*v + z
 		if cmplx.Abs(v) > 2 {
 			return color.RGBA{255 - contrast*n, contrast * n, 50, 255}
-		}
-	}
-	return color.Black
-}
-
-// Some other interesting functions:
-
-func acos(z complex128) color.Color {
-	v := cmplx.Acos(z)
-	blue := uint8(real(v)*128) + 127
-	red := uint8(imag(v)*128) + 127
-	return color.YCbCr{192, blue, red}
-}
-
-func sqrt(z complex128) color.Color {
-	v := cmplx.Sqrt(z)
-	blue := uint8(real(v)*128) + 127
-	red := uint8(imag(v)*128) + 127
-	return color.YCbCr{128, blue, red}
-}
-
-// f(x) = x^4 - 1
-//
-// z' = z - f(z)/f'(z)
-//    = z - (z^4 - 1) / (4 * z^3)
-//    = z - (z - 1/z^3) / 4
-func newton(z complex128) color.Color {
-	const iterations = 37
-	const contrast = 7
-	for i := uint8(0); i < iterations; i++ {
-		z -= (z - 1/(z*z*z)) / 4
-		if cmplx.Abs(z*z*z*z-1) < 1e-6 {
-			return color.Gray{255 - contrast*i}
 		}
 	}
 	return color.Black
