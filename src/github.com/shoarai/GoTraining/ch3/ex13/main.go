89baa0075fbcd2be464f4c8e7ec058d392abeb19
新井昇平 <shohei.arai.52@gmail.com>
2016-03-05 23:59:26 +0900

    add: ex13

---
+++ src/github.com/shoarai/GoTraining/ch3/ex13/main.go
@@ -0,0 +1,30 @@
+// Copyright © 2016 shoarai
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// Netflag demonstrates an integer type used as a bit field.
+package main
+
+import "fmt"
+
+const (
+	_ = 1 << (10 * iota)
+	KB
+	MB
+	GB
+	TB
+	PB
+	EB
+	ZB
+	YB
+)
+
+func main() {
+	fmt.Println("1KB =", KB, "Byte")
+	fmt.Println("1MB =", MB, "Byte")
+	fmt.Println("1GB =", GB, "Byte")
+	fmt.Println("1TB =", TB, "Byte")
+	fmt.Println("1PB =", PB, "Byte")
+	fmt.Println("1EB =", EB, "Byte")
+	fmt.Println("1ZB =", float64(ZB), "Byte")
+	fmt.Println("1YB =", float64(YB), "Byte")
+}
