bd38e191bc1d5b470ca44ccac4a2cd11d21c892f
新井昇平 <shohei.arai.52@gmail.com>
2016-03-06 13:10:39 +0900

    add: sha384, 512

--- src/github.com/shoarai/GoTraining/ch4/ex2/main.go
+++ src/github.com/shoarai/GoTraining/ch4/ex2/main.go
@@ -3,15 +3,20 @@
 
 // The sha command computes the SHA hash (an array) of a string.
 // Example:
-// 	$ go build github.com/shoarai/GoTraining/ch4/ex2/main.go
-//	$ ./main x
-// 	SHA256: 2d711642b726b04401627ca9fbac32f5c8530fb1903cc4db02258717921a4881
-//	$ ./main x -l=384
-//
-//	$ ./main x -l=512
-//
-//	$ ./main x -l=111
-// 	Non support length
+// $ go build github.com/shoarai/GoTraining/ch4/ex2/main.go
+// $ ./main x
+// SHA256:
+//  2d711642b726b04401627ca9fbac32f5c8530fb1903cc4db02258717921a4881
+// $ ./main x -l=384
+// SHA384:
+//  d752c2c51fba0e29aa190570a9d4253e44077a058d3297fa3a5630d5bd012622
+//  f97c28acaed313b5c83bb990caa7da85
+// $ ./main x -l=512
+// SHA512:
+//  a4abd4448c49562d828115d13a1fccea927f52b4d5459297f8b43e42da89238b
+//  c13626e43dcb38ddb082488927ec904fb42057443983e88585179d50551afe62
+// $ ./main x -l=111
+// Non support length
 //
 package main
 
@@ -20,7 +25,10 @@ import (
 	"fmt"
 )
 
-import "crypto/sha256"
+import (
+	"crypto/sha256"
+	"crypto/sha512"
+)
 
 func main() {
 	var len int
@@ -28,23 +36,15 @@ func main() {
 	flag.Parse()
 
 	for _, v := range flag.Args() {
-		c, ok := sha(v, len)
-		if ok {
-			fmt.Printf("SHA%d: %x\n", len, c)
-		} else {
+		switch len {
+		case 256:
+			fmt.Printf("SHA%d: %x\n", len, sha256.Sum256([]byte(v)))
+		case 384:
+			fmt.Printf("SHA%d: %x\n", len, sha512.Sum384([]byte(v)))
+		case 512:
+			fmt.Printf("SHA%d: %x\n", len, sha512.Sum512([]byte(v)))
+		default:
 			fmt.Println("No support length")
 		}
-	}
-}
-
-func sha(x string, len int) ([32]uint8, bool) {
-	switch len {
-	case 256:
-		return sha256.Sum256([]byte(x)), true
-	// case 384:
-	// 	return sha512.Sum384([]byte(x)), true
-	default:
-		var c [32]uint8
-		return c, false
 	}
 }
