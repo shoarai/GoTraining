79a11ed685b82e70cce00abc4539007bdabe09f5
新井昇平 <shohei.arai.52@gmail.com>
2016-03-06 18:27:20 +0900

    clean: remove in slice

--- src/github.com/shoarai/GoTraining/ch4/ex5/rmrepeated/rmrepeated.go
+++ src/github.com/shoarai/GoTraining/ch4/ex5/rmrepeated/rmrepeated.go
@@ -6,15 +6,23 @@ package rmrepeated
 
 // RemoveRepeated rotates a slice of ints in place.
 func RemoveRepeated(str []string) []string {
-	if len(str) < 0 {
+	if len(str) < 1 {
 		return str
 	}
 
-	s := []string{str[0]}
-	for _, v := range str[1:] {
-		if s[len(s)-1] != v {
-			s = append(s, v)
+	for i := 1; i < len(str); i++ {
+		if str[i-1] == str[i] {
+			str, _ = remove(str, i)
 		}
 	}
-	return s
+	return str
+}
+
+func remove(slice []string, i int) ([]string, bool) {
+	if i < 0 || i >= len(slice) {
+		var s []string
+		return s, false
+	}
+	copy(slice[i:], slice[i+1:])
+	return slice[:len(slice)-1], true
 }
