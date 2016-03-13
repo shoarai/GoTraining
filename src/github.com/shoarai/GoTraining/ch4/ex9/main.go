dc2cff4c99f56a21944617c549db727173b02764
新井昇平 <shohei.arai.52@gmail.com>
2016-03-13 14:26:39 +0900

    add: ex9

---
+++ src/github.com/shoarai/GoTraining/ch4/ex9/main.go
@@ -0,0 +1,40 @@
+// Copyright © 2016 shoarai
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// wordfeq computes counts of Unicode words.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Input file path")
+		return
+	}
+
+	path := os.Args[1]
+	f, err := os.Open(path)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	counts := make(map[string]int)
+	input := bufio.NewScanner(f)
+	input.Split(bufio.ScanWords)
+
+	for input.Scan() {
+		text := strings.ToLower(input.Text())
+		counts[text]++
+	}
+
+	fmt.Printf("count\tword\n")
+	for c, n := range counts {
+		fmt.Printf("%d\t%q\n", n, c)
+	}
+}
