4ef819b6c499bfe4424ddfc0226bedc37b9acd13
新井昇平 <shohei.arai.52@gmail.com>
2016-03-20 19:28:25 +0900

    add: ex8 has only complex64

---
+++ src/github.com/shoarai/GoTraining/ch3/ex8/main.go
@@ -0,0 +1,60 @@
+// Copyright © 2016 shoarai
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// Mandelbrot emits a PNG image of the Mandelbrot fractal in full color.
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"math/cmplx"
+	"os"
+)
+
+func main() {
+	const (
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
+		width, height          = 1024, 1024
+	)
+
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < height; py++ {
+		y := float64(py)/height*(ymax-ymin) + ymin
+		for px := 0; px < width; px++ {
+			x := float64(px)/width*(xmax-xmin) + xmin
+			z := complex(x, y)
+			// Image point (px, py) represents complex value z.
+			img.Set(px, py, mandelbrot64(complex64(z)))
+		}
+	}
+	png.Encode(os.Stdout, img) // NOTE: ignoring errors
+}
+
+func mandelbrot64(z complex64) color.Color {
+	const iterations = 200
+	const contrast = 15
+
+	var v complex64
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(complex128(v)) > 2 {
+			return color.RGBA{255 - contrast*n, contrast * n, 50, 255}
+		}
+	}
+	return color.Black
+}
+
+func mandelbrot128(z complex128) color.Color {
+	const iterations = 200
+	const contrast = 15
+
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(v) > 2 {
+			return color.RGBA{255 - contrast*n, contrast * n, 50, 255}
+		}
+	}
+	return color.Black
+}
