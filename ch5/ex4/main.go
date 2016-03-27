ba419b7b5df6f3e1ec2b037e04d4e46d40fcd150
新井昇平 <shohei.arai.52@gmail.com>
2016-03-27 14:08:43 +0900

    add: ch5/ex4

---
+++ ch5/ex4/main.go
@@ -0,0 +1,47 @@
+// Copyright © 2016 shoarai
+
+// Findlinks1 prints the links in an HTML document read from standard input.
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+	visit(doc)
+}
+
+// visit appends to links each link found in n and returns the result.
+func visit(n *html.Node) {
+	if n.Type == html.ElementNode {
+		switch n.Data {
+		case "a":
+			fallthrough
+		case "link":
+			for _, a := range n.Attr {
+				if a.Key == "href" {
+					fmt.Println(a.Val)
+				}
+			}
+		case "script":
+			fallthrough
+		case "img":
+			for _, a := range n.Attr {
+				if a.Key == "src" {
+					fmt.Println(a.Val)
+				}
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		visit(c)
+	}
+}
