d7164a0ebff7e9d55d5c80a8958e74bb05fb1825
新井昇平 <shohei.arai.52@gmail.com>
2016-04-09 16:09:44 +0900

    add: ex8

---
+++ ch5/ex8/main.go
@@ -0,0 +1,92 @@
+// Copyright © 2016 shoarai
+
+// Outline prints the outline of an HTML document tree.
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		outline(url)
+	}
+}
+
+func outline(url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	forEachNode(doc, startElement, endElement)
+
+	return nil
+}
+
+// forEachNode calls the functions pre(x) and post(x) for each node
+// x in the tree rooted at n. Both functions are optional.
+// pre is called before the children are visited (preorder) and
+// post is called after (postorder).
+func forEachNode(n *html.Node, pre, post func(n *html.Node) bool) {
+	if pre != nil {
+		if !pre(n) {
+			return
+		}
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+
+	if post != nil {
+		if !post(n) {
+			return
+		}
+	}
+}
+
+var depth int
+
+func startElement(n *html.Node) bool {
+	if n.Type == html.ElementNode {
+		fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
+		depth++
+	}
+	return true
+}
+
+func endElement(n *html.Node) bool {
+	if n.Type == html.ElementNode {
+		depth--
+		fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
+	}
+	return true
+}
+
+// ElementByID finds element by id
+func ElementByID(doc *html.Node, id string) *html.Node {
+	var node *html.Node
+	forEachNode(doc, func(n *html.Node) bool {
+		if n.Type == html.ElementNode {
+			for _, a := range n.Attr {
+				if a.Key == "id" && a.Val == id {
+					node = n
+					return false
+				}
+			}
+		}
+		return true
+	}, nil)
+	return node
+}
