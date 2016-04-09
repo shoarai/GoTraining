fc6488fa65ac71e8a9a942f26a5f8fa0ef035232
新井昇平 <shohei.arai.52@gmail.com>
2016-04-09 23:42:53 +0900

    add: ex14

---
+++ ch5/ex14/main.go
@@ -0,0 +1,55 @@
+// Copyright © 2016 shoarai
+
+// Findlinks3 crawls the web, starting with the URLs on the command line.
+package main
+
+import "fmt"
+
+// prereqs maps computer science courses to their prerequisites.
+var prereqs = map[string][]string{
+	"algorithms": {"data structures"},
+	"calculus":   {"linear algebra"},
+
+	"compilers": {
+		"data structures",
+		"formal languages",
+		"computer organization",
+	},
+
+	"data structures":       {"discrete math"},
+	"databases":             {"data structures"},
+	"discrete math":         {"intro to programming"},
+	"formal languages":      {"discrete math"},
+	"networks":              {"operating systems"},
+	"operating systems":     {"data structures", "computer organization"},
+	"programming languages": {"data structures", "computer organization"},
+}
+
+// breadthFirst calls f for each item in the worklist.
+// Any items returned by f are added to the worklist.
+// f is called at most once for each item.
+func breadthFirst(f func(item string) []string, worklist []string) {
+	seen := make(map[string]bool)
+	for len(worklist) > 0 {
+		items := worklist
+		worklist = nil
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				worklist = append(worklist, f(item)...)
+			}
+		}
+	}
+}
+
+func main() {
+	var ss []string
+	for k := range prereqs {
+		ss = append(ss, k)
+	}
+
+	breadthFirst(func(s string) []string {
+		fmt.Println(s)
+		return prereqs[s]
+	}, ss)
+}
