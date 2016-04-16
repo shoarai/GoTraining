9ebde4fb586cf21e3f781b32443a2665dc5aabec
新井昇平 <shohei.arai.52@gmail.com>
2016-04-16 20:31:46 +0900

    add: ex5

---
+++ ch6/ex5/main.go
@@ -0,0 +1,54 @@
+// Copyright © 2016 shoarai
+
+package main
+
+import (
+	"fmt"
+
+	"./intset"
+)
+
+func main() {
+	Example_one()
+	Example_two()
+}
+
+func Example_one() {
+	var x, y intset.IntSet
+	x.Add(1)
+	x.Add(144)
+	x.Add(9)
+	fmt.Println(x.String()) // "{1 9 144}"
+
+	y.Add(9)
+	y.Add(42)
+	fmt.Println(y.String()) // "{9 42}"
+
+	x.UnionWith(&y)
+	fmt.Println(x.String()) // "{1 9 42 144}"
+
+	fmt.Println(x.Has(9), x.Has(123)) // "true false"
+
+	// Output:
+	// {1 9 144}
+	// {9 42}
+	// {1 9 42 144}
+	// true false
+}
+
+func Example_two() {
+	var x intset.IntSet
+	x.Add(1)
+	x.Add(144)
+	x.Add(9)
+	x.Add(42)
+
+	fmt.Println(&x)         // "{1 9 42 144}"
+	fmt.Println(x.String()) // "{1 9 42 144}"
+	fmt.Println(x)          // "{[4398046511618 0 65536]}"
+
+	// Output:
+	// {1 9 42 144}
+	// {1 9 42 144}
+	// {[4398046511618 0 65536]}
+}
