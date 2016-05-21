fa2d76cee4f2328618e682c9ab064d41ca61ae1c
新井昇平 <shohei.arai.52@gmail.com>
2016-05-21 16:45:13 +0900

    add: ex6

---
+++ ch7/ex6/main.go
@@ -0,0 +1,18 @@
+// Copyright © 2016 shoarai
+
+// Tempflag prints the value of its -temp (temperature) flag.
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"./tempconv"
+)
+
+var temp = tempconv.CelsiusFlag("temp", 20.0, "the temperature")
+
+func main() {
+	flag.Parse()
+	fmt.Println(*temp)
+}
