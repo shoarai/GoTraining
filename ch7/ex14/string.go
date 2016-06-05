5b876af3af4eac7b220ec693aec7b33931eb7571
新井昇平 <shohei.arai.52@gmail.com>
2016-06-05 15:26:14 +0900

    add: ex14 with bug

---
+++ ch7/ex14/string.go
@@ -0,0 +1,60 @@
+// Copyright © 2016 shoarai
+
+// Package eval provides an expression evaluator.
+package eval
+
+import "fmt"
+
+func (v Var) String() string {
+	return string(v)
+}
+
+func (l literal) String() string {
+	return fmt.Sprintf("%f", l)
+}
+
+func (u unary) String() string {
+	switch u.op {
+	case '+', '-':
+		return fmt.Sprintf("%c%s", u.op, u.x)
+	}
+	panic(fmt.Sprintf("unsupported unary operator: %q", u.op))
+}
+
+func (b binary) String() string {
+	switch b.op {
+	case '+', '-', '*', '/':
+		return fmt.Sprintf("%s %c %s", b.x, b.op, b.y)
+	}
+	panic(fmt.Sprintf("unsupported binary operator: %q", b.op))
+}
+
+func (c call) String() string {
+	switch c.fn {
+	case "pow", "sin", "sqrt":
+		str := c.fn + "("
+		for i, v := range c.args {
+			if i != 0 {
+				str += ", "
+			}
+			str += fmt.Sprintf("%s", v)
+		}
+		return str + ")"
+	}
+	panic(fmt.Sprintf("unsupported function call: %s", c.fn))
+}
+
+func (c multiCall) String() string {
+	switch c.fn {
+	case "min":
+		str := c.fn + "("
+		for i, v := range c.args {
+			if i != 0 {
+				str += ", "
+			}
+			str += fmt.Sprintf("%s", v)
+		}
+		return str + ")"
+	}
+	panic(fmt.Sprintf("unsupported function call with multi argument: %s", c.fn))
+}
