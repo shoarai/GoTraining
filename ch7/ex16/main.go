85320bc125c736753f249872e5baf65123b6c6e0
新井昇平 <shohei.arai.52@gmail.com>
2016-06-05 20:50:55 +0900

    add: web server

--- ch7/ex16/main.go
+++ ch7/ex16/main.go
@@ -5,6 +5,9 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
+
+	"./eval"
 )
 
 func main() {
@@ -12,75 +15,27 @@ func main() {
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
-var html = `
-<div>
-	<input type="text" name="answer" value="0">
-</div>
-<table>
-<tr>
-	<td onclick="setValue(7)">7</td>
-	<td onclick="setValue(8)">8</td>
-	<td onclick="setValue(9)">9</td>
-	<td onclick="setValue('*')">*</td>
-</tr>
-<tr>
-	<td onclick="setValue(4)">4</td>
-	<td onclick="setValue(5)">5</td>
-	<td onclick="setValue(6)">6</td>
-	<td onclick="setValue('-')">-</td>
-</tr>
-<tr>
-	<td onclick="setValue(1)">1</td>
-	<td onclick="setValue(2)">2</td>
-	<td onclick="setValue(3)">3</td>
-	<td onclick="setValue('+')">+</td>
-</tr>
-<tr>
-	<td onclick="setValue(0)" colspan="3">0</td>
-	<td>=</td>
-</tr>
-</table>
-
-<script>
-	function setValue(val) {
-		console.log(val);
-		document.myForm.myLine.value = myTotal;
-	}
-</script>
-`
-
 func handler(w http.ResponseWriter, r *http.Request) {
+	inputExpr := r.URL.Query().Get("expr")
 
-	w.Write([]byte(html))
+	expr, err := eval.Parse(inputExpr)
+	if err != nil {
+		w.Write([]byte("Expression parse error"))
+		return
+	}
 
-	// var inputExpr string
-	// fmt.Println("Input expression:")
-	// fmt.Scan(&inputExpr)
-	//
-	// expr, err := eval.Parse(inputExpr)
-	// if err != nil {
-	// 	fmt.Println("Expression parse error")
-	// 	return
-	// }
-	//
-	// env := eval.Env{}
-	//
-	// for {
-	// 	fmt.Println("Input key name:")
-	//
-	// 	var key string
-	// 	fmt.Scan(&key)
-	// 	if key == "" {
-	// 		break
-	// 	}
-	//
-	// 	fmt.Printf("Input value of %s:\n", key)
-	// 	var val float64
-	// 	fmt.Scan(&val)
-	//
-	// 	env[eval.Var(key)] = val
-	// }
-	//
-	// ans := expr.Eval(env)
-	// fmt.Printf("%s = %f\n", expr, ans)
+	env := eval.Env{}
+	for k, v := range r.URL.Query() {
+		if k != "expr" {
+			val, err := strconv.ParseFloat(v[0], 64)
+			if err == nil {
+				env[eval.Var(k)] = val
+			}
+		}
+	}
+
+	ans := expr.Eval(env)
+	ansStr := strconv.FormatFloat(ans, 'f', 4, 64)
+
+	w.Write([]byte(ansStr))
 }
