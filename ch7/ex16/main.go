ab2463a70f00cf1b72bb23a2674cf744c0ffefb8
新井昇平 <shohei.arai.52@gmail.com>
2016-06-08 00:50:49 +0900

    add: ui

--- ch7/ex16/main.go
+++ ch7/ex16/main.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 	"strconv"
@@ -12,12 +13,65 @@ import (
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/calculate", calculate)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	inputExpr := r.URL.Query().Get("expr")
+var html = template.Must(template.New("html").Parse(`
+	<div>
+		<input type="text" name="expr" value="">
+		=<span></span>
+	</div>
+	<table>
+		<tr>
+			<td onclick="setValue(7)">7</td>
+			<td onclick="setValue(8)">8</td>
+			<td onclick="setValue(9)">9</td>
+			<td onclick="setValue('*')">*</td>
+		</tr>
+		<tr>
+			<td onclick="setValue(4)">4</td>
+			<td onclick="setValue(5)">5</td>
+			<td onclick="setValue(6)">6</td>
+			<td onclick="setValue('-')">-</td>
+		</tr>
+		<tr>
+			<td onclick="setValue(1)">1</td>
+			<td onclick="setValue(2)">2</td>
+			<td onclick="setValue(3)">3</td>
+			<td onclick="setValue()"></td>
+		</tr>
+		<tr>
+			<td onclick="setValue(0)" colspan="3">0</td>
+			<td onclick="equal()">=</td>
+		</tr>
+	</table>
+	<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+	<script>
+		function setValue(val) {
+			$input = $('input');
+			$input.val($input.val()+val)
+		}
 
+		function equal() {
+			$.get({
+				url: "calculate?expr=" + $('input').val(),
+				success: function(ans) {
+					$('span').text(ans);
+				}
+			});
+		}
+	</script>
+`))
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if err := html.Execute(w, ""); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func calculate(w http.ResponseWriter, r *http.Request) {
+	inputExpr := r.URL.Query().Get("expr")
 	expr, err := eval.Parse(inputExpr)
 	if err != nil {
 		w.Write([]byte("Expression parse error"))
@@ -37,6 +91,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	ans := expr.Eval(env)
 	ansStr := strconv.FormatFloat(ans, 'f', 4, 64)
 
-	result := inputExpr + "  = " + ansStr
-	w.Write([]byte(result))
+	w.Write([]byte(ansStr))
 }
