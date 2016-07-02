3b567e405e1f577e046ecc9077cb01042f1dd37c
新井昇平 <shohei.arai.52@gmail.com>
2016-07-02 14:44:36 +0900

    add: ex2

---
+++ ch9/ex2/main.go
@@ -0,0 +1,30 @@
+// Copyright © 2016 shoarai
+
+package popcount
+
+import "sync"
+
+var (
+	loadPcOnce sync.Once
+	pc         [256]byte // pc[i] is the population count of i.
+)
+
+func loadPc() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+// PopCount returns the population count (number of set bits) of x.
+func PopCount(x uint64) int {
+	loadPcOnce.Do(loadPc)
+
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
