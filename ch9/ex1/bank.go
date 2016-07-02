209e5f6eb0dc92af1a2bec274385e78e7041adff
新井昇平 <shohei.arai.52@gmail.com>
2016-07-02 13:22:57 +0900

    add: withdraw returns bool

--- ch9/ex1/bank.go
+++ ch9/ex1/bank.go
@@ -7,9 +7,16 @@ var deposits = make(chan int)  // send amount to deposit
 var balances = make(chan int)  // receive balance
 var withdraws = make(chan int) // send amount to withdraw
 
-func Deposit(amount int)  { deposits <- amount }
-func Balance() int        { return <-balances }
-func Withdraw(amount int) { withdraws <- amount }
+func Deposit(amount int) { deposits <- amount }
+func Balance() int       { return <-balances }
+func Withdraw(amount int) bool {
+	balance := Balance()
+	if balance < amount {
+		return false
+	}
+	withdraws <- amount
+	return true
+}
 
 func teller() {
 	var balance int // balance is confined to teller goroutine
@@ -19,9 +26,7 @@ func teller() {
 			balance += amount
 		case balances <- balance:
 		case amount := <-withdraws:
-			if balance-amount >= 0 {
-				balance -= amount
-			}
+			balance -= amount
 		}
 	}
 }
