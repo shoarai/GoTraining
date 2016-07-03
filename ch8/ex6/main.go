286d6071b3c7531d6ae432c19bffad7eefcb8cbb
新井昇平 <shohei.arai.52@gmail.com>
2016-07-03 20:35:32 +0900

    add: max depth

--- ch8/ex6/main.go
+++ ch8/ex6/main.go
@@ -15,25 +15,42 @@ import (
 	"gopl.io/ch5/links"
 )
 
-func crawl(url string) []string {
-	fmt.Println(url)
-	list, err := links.Extract(url)
+type link struct {
+	url   string
+	depth int
+}
+
+func crawl(l link) []link {
+	fmt.Printf("depth: %d, url: %s\n", l.depth, l.url)
+	list, err := links.Extract(l.url)
 	if err != nil {
 		log.Print(err)
 	}
-	return list
+	var ls []link
+	d := l.depth + 1
+	for i := range list {
+		l := link{list[i], d}
+		ls = append(ls, l)
+	}
+	return ls
 }
 
 func main() {
 	var depth int
-	flag.IntVar(&depth, "depth", 0, "epth of crawlled link")
+	flag.IntVar(&depth, "depth", 0, "depth of links")
 	flag.Parse()
 
-	worklist := make(chan []string)  // lists of URLs, may have duplicates
-	unseenLinks := make(chan string) // de-duplicated URLs
+	worklist := make(chan []link)  // lists of URLs, may have duplicates
+	unseenLinks := make(chan link) // de-duplicated URLs
 
 	// Add command-line arguments to worklist.
-	go func() { worklist <- flag.Args() }()
+	go func() {
+		var ls []link
+		for _, v := range flag.Args() {
+			ls = append(ls, link{v, 0})
+		}
+		worklist <- ls
+	}()
 
 	// Create 20 crawler goroutines to fetch each unseen link.
 	for i := 0; i < 20; i++ {
@@ -50,9 +67,11 @@ func main() {
 	seen := make(map[string]bool)
 	for list := range worklist {
 		for _, link := range list {
-			if !seen[link] {
-				seen[link] = true
-				unseenLinks <- link
+			if !seen[link.url] {
+				seen[link.url] = true
+				if link.depth <= depth {
+					unseenLinks <- link
+				}
 			}
 		}
 	}
