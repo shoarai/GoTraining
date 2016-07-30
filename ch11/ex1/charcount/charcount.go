b981b7722e79b506fbd7f419cc29fd6a01031c03
新井昇平 <shohei.arai.52@gmail.com>
2016-07-30 15:16:49 +0900

    add: test for ex1 in ch11

--- ch11/ex1/charcount/charcount.go
+++ ch11/ex1/charcount/charcount.go
@@ -1,7 +1,7 @@
 // Copyright © 2016 shoarai
 
-// Charcount computes counts of Unicode characters.
-package main
+// charcount computes counts of Unicode characters.
+package charcount
 
 import (
 	"bufio"
@@ -12,12 +12,18 @@ import (
 	"unicode/utf8"
 )
 
-func main() {
+type CharCount struct {
+	Counts  map[rune]int
+	UTFlen  [utf8.UTFMax + 1]int
+	Invalid int
+}
+
+func CountChar(r io.Reader) CharCount {
 	counts := make(map[rune]int)    // counts of Unicode characters
 	var utflen [utf8.UTFMax + 1]int // count of lengths of UTF-8 encodings
 	invalid := 0                    // count of invalid UTF-8 characters
 
-	in := bufio.NewReader(os.Stdin)
+	in := bufio.NewReader(r)
 	for {
 		r, n, err := in.ReadRune() // returns rune, nbytes, error
 		if err == io.EOF {
@@ -34,17 +40,8 @@ func main() {
 		counts[r]++
 		utflen[n]++
 	}
-	fmt.Printf("rune\tcount\n")
-	for c, n := range counts {
-		fmt.Printf("%q\t%d\n", c, n)
-	}
-	fmt.Print("\nlen\tcount\n")
-	for i, n := range utflen {
-		if i > 0 {
-			fmt.Printf("%d\t%d\n", i, n)
-		}
-	}
-	if invalid > 0 {
-		fmt.Printf("\n%d invalid UTF-8 characters\n", invalid)
+
+	return CharCount{
+		counts, utflen, invalid,
 	}
 }
---
+++ ch11/ex1/charcount/charcount_test.go
@@ -0,0 +1,72 @@
+// Copyright © 2016 shoarai
+
+// charcount computes counts of Unicode characters.
+package charcount_test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"unicode/utf8"
+
+	"github.com/shoarai/GoTraining/ch11/ex1/charcount"
+)
+
+func TestCountChar(t *testing.T) {
+	tests := []struct {
+		input string
+		want  charcount.CharCount
+	}{
+		{"a", charcount.CharCount{
+			map[rune]int{'a': 1},
+			[utf8.UTFMax + 1]int{0, 1},
+			0},
+		},
+		{"あいあ", charcount.CharCount{
+			map[rune]int{'あ': 2, 'い': 1},
+			[utf8.UTFMax + 1]int{0, 0, 0, 3},
+			0},
+		},
+	}
+
+	for _, test := range tests {
+		reader := strings.NewReader(test.input)
+		charCount := charcount.CountChar(reader)
+
+		if !isSameCharCount(charCount, test.want) {
+			t.Errorf("CountChar(%s) = %v", test.input, charCount)
+			printCharCount(charCount)
+		}
+	}
+}
+
+func isSameCharCount(charCount1, charCount2 charcount.CharCount) bool {
+	for c, n1 := range charCount1.Counts {
+		if n2, ok := charCount2.Counts[c]; !ok && n1 != n2 {
+			return false
+		}
+	}
+	if charCount1.UTFlen != charCount2.UTFlen {
+		return false
+	}
+	if charCount1.Invalid != charCount2.Invalid {
+		return false
+	}
+	return true
+}
+
+func printCharCount(charCount charcount.CharCount) {
+	fmt.Printf("rune\tcount\n")
+	for c, n := range charCount.Counts {
+		fmt.Printf("%q\t%d\n", c, n)
+	}
+	fmt.Print("\nlen\tcount\n")
+	for i, n := range charCount.UTFlen {
+		if i > 0 {
+			fmt.Printf("%d\t%d\n", i, n)
+		}
+	}
+	if charCount.Invalid > 0 {
+		fmt.Printf("\n%d invalid UTF-8 characters\n", charCount.Invalid)
+	}
+}
