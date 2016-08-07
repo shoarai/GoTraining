3734f55a5a646dcfdf54a3a112e2b842bd30a8c1
新井昇平 <shohei.arai.52@gmail.com>
2016-08-07 15:42:40 +0900

    add: withdraw test

--- ch9/ex1/bank_test.go
+++ ch9/ex1/bank_test.go
@@ -10,49 +10,62 @@ import (
 )
 
 func TestBank(t *testing.T) {
-	done := make(chan struct{})
+	tests := []struct {
+		inputs []int
+		want   int
+	}{
+		// Balance is stocked
+		{[]int{100, 200}, 300},
+		{[]int{0, 600, 100}, 1000},
+	}
 
-	// Alice
-	go func() {
-		bank.Deposit(200)
-		fmt.Println("=", bank.Balance())
-		done <- struct{}{}
-	}()
+	for _, test := range tests {
+		done := make(chan struct{})
+		for i := 0; i < len(test.inputs); i++ {
+			go func(input int) {
+				bank.Deposit(input)
+				fmt.Println("=", bank.Balance())
+				done <- struct{}{}
+			}(test.inputs[i])
+		}
 
-	// Bob
-	go func() {
-		bank.Deposit(100)
-		done <- struct{}{}
-	}()
+		// Wait for both transactions.
+		for i := 0; i < len(test.inputs); i++ {
+			<-done
+		}
 
-	// Wait for both transactions.
-	<-done
-	<-done
-
-	if got, want := bank.Balance(), 300; got != want {
-		t.Errorf("Balance = %d, want %d", got, want)
+		if got := bank.Balance(); got != test.want {
+			t.Errorf("Balance = %d, want %d", got, test.want)
+		}
 	}
 }
 
 func TestWithdraw(t *testing.T) {
-	done := make(chan struct{})
+	tests := []struct {
+		input   int
+		want    bool
+		balance int
+	}{
+		// Balance is stocked
+		{200, true, 800},
+		{1000, false, 800},
+		{800, true, 0},
+	}
 
-	go func() {
-		bank.Deposit(200)
-		fmt.Println("=", bank.Balance())
-		done <- struct{}{}
-	}()
+	for _, test := range tests {
+		isSuccessWithdraw := make(chan bool)
+		go func(input int) {
+			g := bank.Withdraw(input)
+			fmt.Println("=", bank.Balance())
+			isSuccessWithdraw <- g
+		}(test.input)
 
-	go func() {
-		bank.Withdraw(400)
-		fmt.Println("=", bank.Balance())
-		done <- struct{}{}
-	}()
+		if got := <-isSuccessWithdraw; got != test.want {
+			t.Errorf("Withdraw(%d) = %t", test.input, got)
+		}
 
-	<-done
-	<-done
-
-	if got := bank.Balance(); got < 0 {
-		t.Errorf("Balance = %d, want more than zero", got)
+		if balance := bank.Balance(); balance != test.balance {
+			t.Errorf("Balance = %d, want %d", balance, test.balance)
+		}
 	}
 }
