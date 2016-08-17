c9146b8385bd907379a9929efd852aa43a7fffdb
新井昇平 <shohei.arai.52@gmail.com>
2016-08-17 15:02:42 +0900

    fix: command names

--- ch8/ex2/ftp/ftp.go
+++ ch8/ex2/ftp/ftp.go
@@ -21,17 +21,16 @@ func HandleConnection(conn net.Conn) {
 
 	ftpConn := FtpConn{conn, "./"}
 
-	fmt.Fprintf(conn, "> ")
+	fmt.Println("Connected client")
+	ftpConn.reply("220", "FTP server ready")
 
 	input := bufio.NewScanner(conn)
 	for input.Scan() {
 		text := input.Text()
 		err := ftpConn.execute(text)
 		if err != nil {
-			fmt.Fprintln(conn, err)
+			ftpConn.error(err)
 		}
-
-		fmt.Fprintf(conn, "> ")
 	}
 }
 
@@ -39,22 +38,31 @@ func (ftpConn *FtpConn) execute(text string) error {
 	texts := strings.Split(text, " ")
 	cmd := texts[0]
 	args := texts[1:]
+	// NOTE: Print log
+	fmt.Printf("Command: %s, Args: %s\n", cmd, args)
 
-	fmt.Println(cmd, args)
 	switch cmd {
-	case "cd":
+	case "USER":
+		ftpConn.login(args)
+	case "CWD": // cd
 		ftpConn.cd(args)
-	case "ls":
+	case "PORT": // ls
 		ftpConn.list(args)
-	case "get":
+	// case "LIST": // ls
+	// None
+	case "RETR": // get
 		ftpConn.get(args)
-	case "close":
+	case "QUIT": // close
 		ftpConn.close()
 	default:
 		return fmt.Errorf("Invalid command: %s", cmd)
 	}
 
 	return nil
+}
+
+func (ftpConn *FtpConn) login(args []string) {
+	ftpConn.reply("230", "User logged in, proceed.")
 }
 
 func (ftpConn *FtpConn) cd(args []string) {
@@ -72,57 +80,58 @@ func (ftpConn *FtpConn) cd(args []string) {
 	if info.IsDir() {
 		ftpConn.currentDir = path + "/"
 	} else {
-		ftpConn.reply("not a directory")
+		ftpConn.reply("200", "not a directory")
 	}
+
+	ftpConn.reply("200", "Command okay.")
 }
 
 func (ftpConn *FtpConn) list(args []string) {
-	if len(args) == 0 {
-		args = []string{"."}
+	path := ftpConn.currentDir
+	info, err := os.Stat(path)
+	if err != nil {
+		// No such file or directory
+		ftpConn.error(err)
+		return
 	}
 
-	for _, arg := range args {
-		path := ftpConn.currentDir + arg
-		info, err := os.Stat(path)
+	var fileNames []string
+	if info.IsDir() {
+		infos, err := ioutil.ReadDir(path)
 		if err != nil {
-			// No such file or directory
 			ftpConn.error(err)
-			continue
+			return
 		}
-
-		if info.IsDir() {
-			infos, err := ioutil.ReadDir(path)
-			if err != nil {
-				ftpConn.error(err)
-				continue
-			}
-			for _, info := range infos {
-				ftpConn.reply(info.Name())
-			}
-		} else {
-			ftpConn.reply(info.Name())
+		for _, info := range infos {
+			fileNames = append(fileNames, info.Name())
 		}
+	} else {
+		fileNames = append(fileNames, info.Name())
 	}
+
+	ftpConn.reply("200", strings.Join(fileNames, ", "))
 }
 
 func (ftpConn *FtpConn) get(args []string) {
-	for _, arg := range args {
-		b, err := ioutil.ReadFile(arg)
-		if err != nil {
-			ftpConn.error(err)
-			continue
-		}
-
-		ftpConn.reply(string(b))
+	if len(args) == 0 {
+		ftpConn.error(fmt.Errorf("no argument"))
+		return
 	}
+
+	b, err := ioutil.ReadFile(args[0])
+	if err != nil {
+		ftpConn.error(err)
+		return
+	}
+	ftpConn.reply("200", string(b))
 }
 
 func (ftpConn *FtpConn) close() {
 	ftpConn.conn.Close()
 }
 
-func (ftpConn *FtpConn) reply(msg string) {
-	fmt.Fprintf(ftpConn.conn, "%s\n", msg)
+func (ftpConn *FtpConn) reply(code, msg string) {
+	fmt.Fprintf(ftpConn.conn, "%s %s\n", code, msg)
 }
 
 func (ftpConn *FtpConn) error(err error) {
